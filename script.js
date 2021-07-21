e8eb7c18495eafd565b89d89443af4986683c8e5
crstnn <54381541+crstnn@users.noreply.github.com>
2021-07-21 22:15:16 +1000

    added data dropdown

--- script.js
+++ script.js
@@ -179,3 +179,18 @@ fig.on('plotly_legendclick', function(clickData) {
 
 })
 
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
