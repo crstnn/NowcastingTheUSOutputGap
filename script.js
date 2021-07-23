7c9278db1faf4a27535cada0f91562a83820d5f8
crstnn <54381541+crstnn@users.noreply.github.com>
2021-07-23 23:07:33 +1000

    table is full working and orientated the correct way

--- script.js
+++ script.js
@@ -7,144 +7,146 @@ recessionColour = "#DDDDDD";
 graphDiv = "graph";
 
 
-function getAPIData(url){
-    var req = new XMLHttpRequest();
-    req.open("GET", url, false);
-    req.send(null);
-    return req.responseText;
+function getAPIData(url) {
+  var req = new XMLHttpRequest();
+  req.open("GET", url, false);
+  req.send(null);
+  return req.responseText;
 }
 
-function getYearAndQuarter(val){
-    q = val % 1
-    d = {
-        0: "Q1",
-        0.25: "Q2",
-        0.5: "Q3",
-        0.75: "Q4"
-    }
-    return String(~~val) + " " + String(d[q])
+function getYearAndQuarter(val) {
+  q = val % 1
+  d = {
+    0: "Q1",
+    0.25: "Q2",
+    0.5: "Q3",
+    0.75: "Q4"
+  }
+  return String(~~val) + " " + String(d[q])
 }
 
 function graph(gapDict) {
 
-    document.getElementById("last_update").innerHTML = "Latest update (UTC): "+ String(gapDict['latestRunUTC']);
+  document.getElementById("last_update").innerHTML = "Latest update (UTC): " + String(gapDict['latestRunUTC']);
 
-      cValList = Object.values(gapDict['concreteObservations']);
-      nValList = Object.values(gapDict['nowcastForecastObservations']);
+  cValList = Object.values(gapDict['concreteObservations']);
+  nValList = Object.values(gapDict['nowcastForecastObservations']);
 
-      concreteYVal = [];
+  concreteYVal = [];
 
-      for (var i = 0; i < cValList.length; i++){
-        concreteYVal.push(cValList[i]['gapPercentage']);
-      }
+  for (var i = 0; i < cValList.length; i++) {
+    concreteYVal.push(cValList[i]['gapPercentage']);
+  }
 
-      nowcastForecastYVal = []
+  nowcastForecastYVal = []
 
-      for (var i = 0; i < nValList.length; i++){
-        nowcastForecastYVal.push(nValList[i]['gapPercentage']);
-      }
+  for (var i = 0; i < nValList.length; i++) {
+    nowcastForecastYVal.push(nValList[i]['gapPercentage']);
+  }
 
-      concreteXVal = Object.keys(gapDict['concreteObservations'])
-      nowcastForcastXVal = Object.keys(gapDict['nowcastForecastObservations'])
-      nowcastXVal = [nowcastForcastXVal[0]]
-      nowcastYVal = [nowcastForecastYVal[0]]
-      forecastXVal = nowcastForcastXVal.slice(Math.max(nowcastForcastXVal.length - 5, 1))
-      forecastYVal = nowcastForecastYVal.slice(Math.max(nowcastForecastYVal.length - 5, 1))
+  concreteXVal = Object.keys(gapDict['concreteObservations'])
+  nowcastForcastXVal = Object.keys(gapDict['nowcastForecastObservations'])
+  nowcastXVal = [nowcastForcastXVal[0]]
+  nowcastYVal = [nowcastForecastYVal[0]]
+  forecastXVal = nowcastForcastXVal.slice(Math.max(nowcastForcastXVal.length - 5, 1))
+  forecastYVal = nowcastForecastYVal.slice(Math.max(nowcastForecastYVal.length - 5, 1))
 
-      yearQuarterText = Object.keys(gapDict['concreteObservations']).map(getYearAndQuarter)
+  yearQuarterText = Object.keys(gapDict['concreteObservations']).map(getYearAndQuarter)
 
-      var traceConcreteObs = {
-        x: concreteXVal,
-        y: concreteYVal,
-        mode: 'lines + markers',
-        name: 'Concrete Estimates',
-        customdata: yearQuarterText,
-        hovertemplate: "%{customdata}, %{y}",
-        line: {color: concreteColour}
-      };
+  var traceConcreteObs = {
+    x: concreteXVal,
+    y: concreteYVal,
+    mode: 'lines + markers',
+    name: 'Concrete Estimates',
+    customdata: yearQuarterText,
+    hovertemplate: "%{customdata}, %{y}",
+    line: { color: concreteColour }
+  };
 
-      var traceNowcast = {
-        x: nowcastXVal,
-        y: nowcastYVal,
-        mode: 'lines + markers',
-        name: 'Nowcast',
-        customdata: nowcastXVal.map(getYearAndQuarter),
-        hovertemplate: "%{customdata}, %{y}",
-        line: {color: nowcastColour}
-      };
+  var traceNowcast = {
+    x: nowcastXVal,
+    y: nowcastYVal,
+    mode: 'lines + markers',
+    name: 'Nowcast',
+    customdata: nowcastXVal.map(getYearAndQuarter),
+    hovertemplate: "%{customdata}, %{y}",
+    line: { color: nowcastColour }
+  };
 
-      var traceForecast = {
-        x: forecastXVal,
-        y: forecastYVal,
-        mode: 'lines + markers',
-        name: 'Conditional Forecast',
-        customdata: forecastXVal.map(getYearAndQuarter),
-        hovertemplate: "%{customdata}, %{y}",
-        visible: "legendonly",
-        line: {color: forecastColour}
-      };
+  var traceForecast = {
+    x: forecastXVal,
+    y: forecastYVal,
+    mode: 'lines + markers',
+    name: 'Conditional Forecast',
+    customdata: forecastXVal.map(getYearAndQuarter),
+    hovertemplate: "%{customdata}, %{y}",
+    visible: "legendonly",
+    line: { color: forecastColour }
+  };
 
-      var layout = {autosize: true,
-                    shapes: [
-                      {
-                        type: 'line',
-                        x0: concreteXVal[concreteXVal.length-1],
-                        y0: concreteYVal[concreteYVal.length-1],
-                        x1: nowcastXVal[0],
-                        y1: nowcastYVal[0],
-                        visible: true,
-                        line: {
-                          color: nowcastColour,
-                          width: 2,
-                        }
-                      },
-                      {
-                        type: 'line',
-                        x0: nowcastXVal[0],
-                        y0: nowcastYVal[0],
-                        x1: forecastXVal[0],
-                        y1: forecastYVal[0],
-                        visible: false,
-                        line: {
-                          color: forecastColour,
-                          width: 2,
-                        }
-                      }
-                    ],
-                    margin: {'l': 35, 'r': 30, 't': 30, 'b': 30},
-                    yaxis: {
-                      ticksuffix: "%"
-                    },
-                    xaxis: {
-                      showgrid: false
-                    },
-                    legend: {
-                        orientation: "h",
-                        yanchor: "top",
-                        y: 1.03,
-                        xanchor: "right",
-                        x: 1}
-                      }
-      
-      recessions.forEach(recessionPeriod => layout['shapes'].push(                      
-        {
-        type: 'rect',
-        xref: 'x',
-        yref: 'paper',
-        x0: recessionPeriod[0],
-        y0: 0,
-        x1: recessionPeriod[1],
-        y1: 1,
-        fillcolor: recessionColour,
-        opacity: 0.2,
+  var layout = {
+    autosize: true,
+    shapes: [
+      {
+        type: 'line',
+        x0: concreteXVal[concreteXVal.length - 1],
+        y0: concreteYVal[concreteYVal.length - 1],
+        x1: nowcastXVal[0],
+        y1: nowcastYVal[0],
+        visible: true,
         line: {
-            width: 0
+          color: nowcastColour,
+          width: 2,
         }
-      }))
-                        
-      const data = [traceConcreteObs, traceNowcast, traceForecast];
+      },
+      {
+        type: 'line',
+        x0: nowcastXVal[0],
+        y0: nowcastYVal[0],
+        x1: forecastXVal[0],
+        y1: forecastYVal[0],
+        visible: false,
+        line: {
+          color: forecastColour,
+          width: 2,
+        }
+      }
+    ],
+    margin: { 'l': 35, 'r': 30, 't': 30, 'b': 30 },
+    yaxis: {
+      ticksuffix: "%"
+    },
+    xaxis: {
+      showgrid: false
+    },
+    legend: {
+      orientation: "h",
+      yanchor: "top",
+      y: 1.03,
+      xanchor: "right",
+      x: 1
+    }
+  }
 
-      Plotly.newPlot(graphDiv, data, layout);
+  recessions.forEach(recessionPeriod => layout['shapes'].push(
+    {
+      type: 'rect',
+      xref: 'x',
+      yref: 'paper',
+      x0: recessionPeriod[0],
+      y0: 0,
+      x1: recessionPeriod[1],
+      y1: 1,
+      fillcolor: recessionColour,
+      opacity: 0.2,
+      line: {
+        width: 0
+      }
+    }))
+
+  const data = [traceConcreteObs, traceNowcast, traceForecast];
+
+  Plotly.newPlot(graphDiv, data, layout);
 
 }
 
@@ -152,7 +154,7 @@ const gapDict = JSON.parse(getAPIData(gapUrl));
 graph(gapDict);
 
 
-function onResize(){
+function onResize() {
   graph(gapDict);
 }
 
@@ -161,12 +163,12 @@ const fig = document.getElementById(graphDiv)
 
 fig.on('plotly_legenddoubleclick', () => false);
 
-fig.on('plotly_legendclick', (clickData) => { 
+fig.on('plotly_legendclick', (clickData) => {
   const curvNum = clickData.curveNumber;
 
-  if ([1, 2].includes(curvNum)){
+  if ([1, 2].includes(curvNum)) {
     var update = {};
-    update["shapes[" + String(curvNum-1) + "].visible"] = clickData.data[curvNum].visible == 'legendonly' ? true : false;
+    update["shapes[" + String(curvNum - 1) + "].visible"] = clickData.data[curvNum].visible == 'legendonly' ? true : false;
 
     Plotly.relayout(graphDiv, update);
   }
@@ -180,11 +182,11 @@ for (var i = 0; i < dataCollapsible.length; i++) {
   dataCollapsible[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-    } 
+    }
   });
 }
 
@@ -192,31 +194,51 @@ const last4MonthsDict = JSON.parse(getAPIData(last4MonthsForNowcastUrl));
 buildTable(last4MonthsDict)
 
 function round(val) {
-  if (val == "None"){return "-"}
+  if (val == "None") { return "-" }
   return Math.round(val * 100) / 100
 }
 
 function buildTable(dataDict) {
-  const last4monthsTable = document.getElementById('dataTable')
-  var dataArray = []
-  for (var key in dataDict["observations"]) {
-    if (dataDict["observations"].hasOwnProperty(key)) {
-      dataArray.push( dataDict["observations"][key] );
-    }
+  month = {
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04": "April",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "August",
+    "09": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December",
   }
 
-  for (var i=0; i < dataArray.length; i++){
-    var row = `<tr>
-    <td>${round(dataArray[i].FEDFUNDS)}</td>
-    <td>${round(dataArray[i].TERMSPREAD)}</td>
-    <td>${round(dataArray[i].RISKSPREAD)}</td>
-    <td>${round(dataArray[i].SP500PERC)}</td>
-    <td>${round(dataArray[i].UMCSENT)}</td>   
-    <td>${round(dataArray[i].UNRATE)}</td>
-    <td>${round(dataArray[i].CPIAUCSLPERC)}</td>
-    <td>${round(dataArray[i].INDPROPERC)}</td>     
-    <td>${round(dataArray[i].HOUSTPERC)}</td>     
-    </tr>`
-    last4monthsTable.innerHTML += row
+  const last4monthsTable = document.getElementById('dataTable')
+  var dataArray = []
+  var keyArray = []
+  for (var key in dataDict["observations"]) {
+    keyArray.push(key)
+    dataArray.push(dataDict["observations"][key]);
+  }
+
+  titles = ["Federal funds rate (%)", "Term spread (%)", "Risk spread (%)", "Stock returns (%)",
+    "Consumer sentiment (indx.)", "Unemployment rate (%)", "Monthly CPI Inflation (%)", "IP growth (%)", "Housing starts growth (%)"]
+  dictKeys = ["FEDFUNDS", "TERMSPREAD", "RISKSPREAD", "SP500PERC", "UMCSENT", "UNRATE", "CPIAUCSLPERC", "INDPROPERC", "HOUSTPERC"]
+
+  var horizontalHeader = "<tr><th> </th>";
+  for (const key of keyArray) {
+    horizontalHeader += "<th>" + month[key.slice(-2)] + "</th>"
+  }
+  horizontalHeader += "</tr><tr>"
+  last4monthsTable.innerHTML += horizontalHeader;
+
+  for (var k = 0; k < dictKeys.length; k++) {
+    row = `<tr><th> ${titles[k]} </th>`
+    for (var i = 0; i < dataArray.length; i++) {
+      row += `<td>${round(dataArray[i][dictKeys[k]])}</td>`
+    }
+    row += "</tr>"
+    last4monthsTable.innerHTML += row;
   }
 }
