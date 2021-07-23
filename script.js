0e65811ebafcd476418b2a63e64b3ee2f59eb7b2
crstnn <54381541+crstnn@users.noreply.github.com>
2021-07-23 13:31:53 +1000

    separated out recession data

--- script.js
+++ script.js
@@ -125,15 +125,6 @@ function graph(gapDict) {
                         x: 1}
                       }
       
-      const recessions = [[1969.75, 1970.75],
-      [1973.75, 1975.0],
-      [1980.0, 1980.5],
-      [1981.5, 1982.75],
-      [1990.5, 1991.0],
-      [2001.0, 2001.75],
-      [2007.75, 2009.25],
-      [2020.0, 2020.25]] 
-
       recessions.forEach(recessionPeriod => layout['shapes'].push(                      
         {
         type: 'rect',
