76233677df28229db9c221369c60f2e4411576e0
crstnn <54381541+crstnn@users.noreply.github.com>
2021-10-16 10:20:47 +1100

    abbreviated table dates

--- script.js
+++ script.js
@@ -277,18 +277,18 @@ function roundSpecial(value, isTo1DP=False) {
 
 function buildTable(dataDict) {
   const month = {
-    "01": "January",
-    "02": "February",
-    "03": "March",
-    "04": "April",
-    "05": "May",
-    "06": "June",
-    "07": "July",
-    "08": "August",
-    "09": "September",
-    "10": "October",
-    "11": "November",
-    "12": "December",
+    "01": "Jan.",
+    "02": "Feb.",
+    "03": "Mar.",
+    "04": "Apr.",
+    "05": "May.",
+    "06": "Jun.",
+    "07": "Jul.",
+    "08": "Aug.",
+    "09": "Sept.",
+    "10": "Oct.",
+    "11": "Nov.",
+    "12": "Dec.",
   };
 
   const last4monthsTable = document.getElementById('dataTable');
