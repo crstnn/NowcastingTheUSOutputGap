6e4da56eb06d9a464d53df36528afc77c4ded0be
crstnn <54381541+crstnn@users.noreply.github.com>
2022-02-01 18:31:29 +1100

    consistent pascal case

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function writeTextBelowGraph(reqJSON) {
 
 async function graph(reqJSON) {
 
-    document.getElementById("last_update").innerHTML = "Last updated (UTC): " + reqJSON['latestRunUTC'].slice(0, reqJSON['latestRunUTC'].length - 3);
+    document.getElementById("lastUpdate").innerHTML = "Last updated (UTC): " + reqJSON['latestRunUTC'].slice(0, reqJSON['latestRunUTC'].length - 3);
 
     const
         cValList = Object.values(reqJSON['concreteObservations']),
